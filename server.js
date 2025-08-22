5f1ff8fc01d8e13d42f7493a58101c9718eb8e35
tncks <duddndtncks@naver.com>
2025-08-22 17:20:26 +0900

    both update

--- server.js
+++ server.js
@@ -31,10 +31,10 @@ async function fetchAndInsertStockData() {
         const params = {
             serviceKey: serviceKey,
             resultType: 'json',
-            beginBasDt: '20250101',
+            beginBasDt: '20220101',
             endBasDt: '20250822',
             likeSrtnCd: code,
-            numOfRows: 120,
+            numOfRows: 1200,
             pageNo: 1
         };
         return axios.get(apiUrl, { params });
